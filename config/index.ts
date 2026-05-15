c707fb3b81367101e23e0f126aa40706ceadcdb3
tizul-islam <tizulislamtt@gmail.com>
2026-05-15 20:17:58 +0600

    feat: initialize configuration module to manage environment variables

--- config/index.ts
+++ config/index.ts
@@ -6,4 +6,4 @@ const config ={
     connection_string: process.env.DATABASE_URL as string,
     port : process.env.PORT as unknown as number,
 }
-export default config 
+export default config       
